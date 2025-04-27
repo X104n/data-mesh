5b0ed522624375d285c90c505497e4778ed3a62e
X104n <github.freeware171@passmail.net>
2025-04-27 18:49:19 +0200

    Removed sleep after consume

--- src/domain_app.py
+++ src/domain_app.py
@@ -205,8 +205,6 @@ if __name__ == "__main__":
         print(f"Product: {product}")
 
         time_keeping(start_time, True)
-
-        time.sleep(1)
         
     '''
     Make sure the server do not exit
