995fa4e6dfa92cddd8d6f2644a0f8bfb71deae0d
X104n <github.freeware171@passmail.net>
2025-05-16 17:12:36 +0200

    100_000 runs

--- src/domain_app.py
+++ src/domain_app.py
@@ -151,7 +151,7 @@ if __name__ == "__main__":
     '''
     input("Press Enter to start consuming products from the mesh...")
 
-    for i in range(0, 10_000):
+    for i in range(0, 100_000):
         print("Consume product start")
 
         start_time = time.time()
