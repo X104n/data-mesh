01f9f38cd2bcb72f653ec5baebfa68e7935e5f51
X104n <github.freeware171@passmail.net>
2025-05-19 20:58:41 +0200

    Can now read the last n lines of file

--- src/test.py
+++ src/test.py
@@ -1,9 +1,47 @@
-import csv
-from collections import deque
+def read_last_n_lines(f, n=1):
+        f.seek(0, 2)  # Go to the end of the file
+        size = f.tell()
+        
+        if size == 0:  # Empty file
+            return []
+            
+        # Start from the end
+        position = size - 1
+        newlines_found = 0
+        
+        # Find the start of the nth-to-last line
+        while position >= 0:
+            f.seek(position)
+            char = f.read(1)
+            if char == b'\n':
+                newlines_found += 1
+                if newlines_found == n and position > 0:
+                    # Found the nth newline from the end
+                    position += 1
+                    break
+            position -= 1
+            
+        # If we went all the way to the beginning, start from there
+        if position < 0:
+            position = 0
+            
+        # Read the last n lines
+        f.seek(position)
+        last_chunk = f.read().decode('utf-8')
+        last_lines = last_chunk.splitlines()
+        
+        # If we have more lines than requested, return only the last n
+        if len(last_lines) > n:
+            return last_lines[-n:]
+        return last_lines
 
-with open("src/domain_app.csv", "r") as f:
-        lines = deque(f, 1_000)
-print(lines)
-print("==================================")
-last_lines = [line.strip().split(";") for line in lines]
-print(last_lines)
+
+file = open("test_log.txt", "rb")
+# Example usage
+last_2_lines = read_last_n_lines(file, 2)
+print("Last 2 lines:", last_2_lines)
+
+last_3_lines = read_last_n_lines(file, 3)
+print("Last 3 lines:", last_3_lines)
+
+file.close()
